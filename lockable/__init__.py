f1caee9971d7a954c322870395fe46a33cb15b0a
Jussi Vatjus-Anttila <jussiva@gmail.com>
2020-10-02 07:57:07 +0300

    add missing import

--- lockable/__init__.py
+++ lockable/__init__.py
@@ -1,1 +1,2 @@
-from lockable.lockable import read_resources_list, parse_requirements, lock, validate_json, ResourceNotFound
+from lockable.lockable import read_resources_list, parse_requirements, lock, \
+    validate_json, ResourceNotFound, get_requirements
