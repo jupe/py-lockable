c2507c833ebcd85b871ce27f10239e7b1a2f182e
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-05-14 07:46:44 +0300

    add validate-only option for cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -39,3 +39,25 @@ class LockableCliTests(TestCase):
                 with patch.object(sys, 'argv', testargs):
                     main()
             self.assertEqual(cm.exception.code, 0)
+
+    def test_validate_only_fail(self):
+        with TemporaryDirectory() as tmpdirname:
+            list_file = os.path.join(tmpdirname, 'resources.json')
+            with open(list_file, 'w') as fp:
+                fp.write('[{"id": "abc", "hostname": "localhost", "online": true},'
+                         ' {"id": "abc", "hostname": "localhost2", "online": true}]')
+            testargs = ["prog", "--validate-only", "--hostname", "localhost", "--resources", list_file, "echo", "$ID"]
+            with self.assertRaises(ValueError):
+                with patch.object(sys, 'argv', testargs):
+                    main()
+
+    def test_validate_only_ok(self):
+        with TemporaryDirectory() as tmpdirname:
+            list_file = os.path.join(tmpdirname, 'resources.json')
+            with open(list_file, 'w') as fp:
+                fp.write('[{"id": "abc", "hostname": "localhost", "online": true}]')
+            testargs = ["prog", "--validate-only", "--hostname", "localhost", "--resources", list_file, "echo", "$ID"]
+            with self.assertRaises(SystemExit) as cm:
+                with patch.object(sys, 'argv', testargs):
+                    main()
+            self.assertEqual(cm.exception.code, 0)
