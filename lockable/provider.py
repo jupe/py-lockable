a4ebaa908a450df65da731b41d99eb2c7bd8a3b6
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-08-10 13:29:09 +0300

    Update provider.py

--- lockable/provider.py
+++ lockable/provider.py
@@ -18,7 +18,7 @@ class ProviderError(Exception):
 
 class Provider(ABC):
     """ Abstract Provider """
-    def __init__(self, uri: typing.Union[str, List[Dict]]):
+    def __init__(self, uri: typing.Union[str, list]):
         """ Provider constructor """
         self._uri = uri
         self._resources = list()
