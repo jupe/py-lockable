cbd9a80352712c48fe9ff4d9cb74eda552baedc5
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-08-15 22:10:03 +0300

    drafting http retry

--- lockable/provider_http.py
+++ lockable/provider_http.py
@@ -2,6 +2,9 @@
 import logging
 import requests
 from requests import HTTPError
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from requests.packages.urllib3.util import parse_url
 
 from lockable.provider import Provider, ProviderError
 
@@ -14,18 +17,42 @@ class ProviderHttp(Provider):
     def __init__(self, uri: str):
         """ ProviderHttp constructor """
         super().__init__(uri)
+        self._configure_http_strategy()
         self._reload()
+
+    def _configure_http_strategy(self):
+        """ configure http Strategy """
+        retry_strategy = Retry(
+            total=3,
+            status_forcelist=[
+                429,  # Too Many Requests
+                500,  # Internal Server Error
+                502,  # Bad Gateway server error
+                503,  # Service Unavailable
+                504  # Gateway Timeout server error
+            ],
+            method_whitelist=["HEAD", "GET", "OPTIONS"]
+        )
+
+        #  create http adapter with retry strategy
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self._http = requests.Session()
+
+        # Assert in hook if not success response
+        assert_status_hook = (lambda response, *args, **kwargs: response.raise_for_status())
+        self._http.hooks["response"] = [assert_status_hook]
+
+        url = parse_url(self._uri)
+        self._http.mount(f'{url.scheme}://', adapter)
 
     def _reload(self) -> None:
         """ Reload resources list from web server """
         self.set_resources_list(self._get_http(self._uri))
 
-    @staticmethod
-    def _get_http(uri: str) -> list:
+    def _get_http(self) -> list:
         """ Internal method to get http json data"""
         try:
-            response = requests.get(uri)
-            response.raise_for_status()
+            response = self._http.get(self.uri)
 
             # could utilise ETag or Last-Modified headers to optimize performance
             # etag = response.headers.get("ETag")
