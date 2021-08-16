5b3b5802c647529c5fc3761a929e6b9d2a3ce1d6
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-08-16 21:15:28 +0300

    fix conflict

--- lockable/provider_http.py
+++ lockable/provider_http.py
@@ -2,6 +2,10 @@
 import logging
 import requests
 from requests import HTTPError
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from urllib3.util import parse_url
+
 from lockable.provider import Provider, ProviderError
 
 MODULE_LOGGER = logging.getLogger('lockable')
@@ -12,7 +16,34 @@ class ProviderHttp(Provider):
 
     def __init__(self, uri: str):
         """ ProviderHttp constructor """
+        self._configure_http_strategy(uri)
         super().__init__(uri)
+
+    def _configure_http_strategy(self, uri):
+        """ configure http Strategy """
+        retry_strategy = Retry(
+            total=5,
+            redirect=5,
+            status_forcelist=[
+                429,  # Too Many Requests
+                500,  # Internal Server Error
+                502,  # Bad Gateway server error
+                503,  # Service Unavailable
+                504  # Gateway Timeout server error
+            ],
+            backoff_factor=0.5
+        )
+
+        #  create http adapter with retry strategy
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self._http = requests.Session()
+
+        # Assert in hook if not success response
+        assert_status_hook = (lambda response, *args, **kwargs: response.raise_for_status())
+        self._http.hooks["response"] = [assert_status_hook]
+
+        url = parse_url(uri)
+        self._http.mount(f'{url.scheme}://', adapter)
 
     def reload(self) -> None:
         """ Reload resources list from web server """
@@ -22,8 +53,7 @@ class ProviderHttp(Provider):
     def _get_http(uri: str) -> list:
         """ Internal method to get http json data"""
         try:
-            response = requests.get(uri)
-            response.raise_for_status()
+            response = self._http.get(uri)
 
             # could utilise ETag or Last-Modified headers to optimize performance
             # etag = response.headers.get("ETag")
